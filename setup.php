a4cc3697b45a909aa8f6846ce7879e7c57d840f6
Roy Brannath <14164020+rbrannath@users.noreply.github.com>
2022-02-12 10:29:25 +0100

    Create setup.php

---
+++ setup.php
@@ -0,0 +1,13 @@
+<?
+ 
+/**
+ * Get the name and the version of the plugin - Needed
+ */
+function plugin_version_networkexplorer() {
+   return array('name'           => "Network Explorer",
+                'version'        => '0.0.1',
+                'author'         => 'Roy Brannath',
+                'license'        => 'GPLv3+',
+                'homepage'       => 'https://github.com/rbrannath/networkexplorer',
+                'minGlpiVersion' => '9.5');
+}
