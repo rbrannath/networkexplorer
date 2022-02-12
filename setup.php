18d5b26e7d106046537b5462fcb741962eaae5dc
Roy Brannath <14164020+rbrannath@users.noreply.github.com>
2022-02-12 10:31:53 +0100

    added check_config and check_prerequisites

--- setup.php
+++ setup.php
@@ -11,3 +11,24 @@ function plugin_version_networkexplorer() {
                 'homepage'       => 'https://github.com/rbrannath/networkexplorer',
                 'minGlpiVersion' => '9.5');
 }
+
+/**
+ *  Check if the config is ok - Needed
+ */
+function plugin_networkexplorer_check_config() {
+    return true;
+}
+ 
+/**
+ * Check if the prerequisites of the plugin are satisfied - Needed
+ */
+function plugin_networkexplorer_check_prerequisites() {
+ 
+    // Check that the GLPI version is compatible
+    if (version_compare(GLPI_VERSION, '9.5', 'lt') || version_compare(GLPI_VERSION, '9.5', 'gt')) {
+        echo "This plugin Requires GLPI >= 9.5 and GLPI <9.5";
+        return false;
+    }
+ 
+    return true;
+}
