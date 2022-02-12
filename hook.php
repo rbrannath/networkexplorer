2f95e824295c5cfc93ed5955ac46723f24366e3e
Roy Brannath <14164020+rbrannath@users.noreply.github.com>
2022-02-12 10:30:12 +0100

    Create hook.php

---
+++ hook.php
@@ -0,0 +1,11 @@
+<?
+ 
+/**
+ * Called when user click on Install - Needed
+ */
+function plugin_networkexplorer_install() { return true; }
+ 
+/**
+ * Called when user click on Uninstall - Needed
+ */
+function plugin_networkexplorer_uninstall() { return true; }
